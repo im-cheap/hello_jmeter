17653c0acc4bad65dcb09c6c7908a0eef2cbd92c
jng1x <jng@renewfinancial.com>
2016-02-19 11:29:52 -0800

    adding rake task

--- Rakefile
+++ Rakefile
@@ -1,3 +1,7 @@
+require "rake/test"
+
 task :test do
    ruby "test/goToGoogle.rb"
 end
+
+task :default => [:test]
--- Rakefile.rb
+++
@@ -1,9 +0,0 @@
-task :default => [:test]
-
-#task :test do
-#  ruby "test/unittest.rb"
-#end
-
-task :test do
-   Dir[File.expand_path "test/**/*.rb"].each{|f| require_relative(f)}
-end
