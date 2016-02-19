cb20429c167c1292ae9a19e4fc26b4b3592adc0a
jng1x <jng@renewfinancial.com>
2016-02-19 11:53:30 -0800

    updated goToGoogle test file

--- test/goToGoogle.rb
+++ test/goToGoogle.rb
@@ -1,8 +1,18 @@
 require 'rubygems'
 require 'ruby-jmeter'
 
-test do
-  threads count: 1 do
-    visit name: 'Home Page', url: 'http://google.com/'
+#test do
+#  threads count: 1 do
+#    visit name: 'Home Page', url: 'http://google.com/'
+#  end
+#end
+
+
+
+test do  
+  with_json
+  threads 1, loops: 5 do
+    get name: 'get_products_index',
+        url: 'http://example-rest-api.herokuapp.com/api/products'
   end
-end
+end.run 
